838b5155a0d0e884986347479c9e935fee50888e
SirCarding <104885203+SirCarding@users.noreply.github.com>
2022-05-04 15:27:26 +0800

    Add files via upload

---
+++ ListFragment.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mobi.Activities
+{
+    [Activity(Label = "ListFragment")]
+    public class ListFragment : AndroidX.Fragment.App.Fragment
+    {
+
+
+
+        private JavaList<Rice> rice;
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+         
+            View view = inflater.Inflate(Resource.Layout.list_layout, container, false);
+            var ListViewItem = view.FindViewById<ListView>(Resource.Id.listView1);
+
+           //ListView Bind for Rice Items
+            ListViewItem.Adapter = new CustomAdapter(this.Context as Activity, GetRices());
+
+            return view;
+
+                         
+           }
+
+        private JavaList<Rice> GetRices()
+        {
+            rice = new JavaList<Rice>();
+            Rice s;
+
+            s = new Rice("Basmati", Resource.Drawable.basmati_longrice);
+            rice.Add(s);
+           
+            s = new Rice("Jasponica", Resource.Drawable.basmati_longrice);
+            rice.Add(s);
+           
+            s = new Rice("Sakura", Resource.Drawable.basmati_longrice);
+            rice.Add(s);
+
+            s = new Rice("Spanish Gate", Resource.Drawable.basmati_longrice);
+            rice.Add(s);
+
+            s = new Rice("Rice 5", Resource.Drawable.basmati_longrice);
+            rice.Add(s);
+
+            s = new Rice("Rice 6", Resource.Drawable.basmati_longrice);
+            rice.Add(s);
+
+            s = new Rice("Rice 7", Resource.Drawable.basmati_longrice);
+            rice.Add(s);
+
+            s = new Rice("Rice 8", Resource.Drawable.basmati_longrice);
+            rice.Add(s);
+
+            return rice;
+        }
+
+          
+     }
+ }
---
+++ UserEventListener.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Database;
+using mobi.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mobi.Listeners
+{
+    public class UserEventListener : Java.Lang.Object, IValueEventListener
+    {
+        ISharedPreferences preferences = Application.Context.GetSharedPreferences("userinfo", FileCreationMode.Private);
+        ISharedPreferencesEditor editor;
+        public void OnCancelled(DatabaseError error)
+        {
+           
+        }
+
+        public void OnDataChange(DataSnapshot snapshot)
+        {
+           if(snapshot != null)
+            {
+                string fullname, email, phone = "";
+                fullname = (snapshot.Child("fname") != null) ? snapshot.Child("fname").Value.ToString() : "";
+                email = (snapshot.Child("email") != null) ? snapshot.Child("email").Value.ToString() : "";
+                if(snapshot.Child("phone") != null)
+                {
+                    phone = snapshot.Child("phone").Value.ToString();
+                }
+
+                editor.PutString("fullname",fullname);
+                editor.PutString("email", email);
+                editor.PutString("phone", phone);
+                editor.Apply();
+            }
+        }
+
+        public void Create()
+        {
+            editor = preferences.Edit();
+            FirebaseDatabase database = AppDataHelper.GetDatabase();
+            string userID = AppDataHelper.GetCurrentUser().Uid;
+
+            DatabaseReference profileReferece = database.GetReference("users/" + userID);
+            profileReferece.AddValueEventListener(this);
+        }
+    }
+}
